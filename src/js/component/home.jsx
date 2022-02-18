02cd223b2a17629c46dcb3a7481fe53bef8fe41a
Luis Piñero Quintana <lpineroq@gmail.com>
2022-02-18 16:15:31 +0000

    commit 5

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { Button, Form } from "react-bootstrap";
+import TodoList from "./todoList.jsx";
 
 //create your first component
 const Home = () => {
@@ -10,9 +11,13 @@ const Home = () => {
 
 	const handleClick = () => {
 		console.log("Click");
-		/*
-		const newListTodo = [...listTodo, newTodo]; */
-		setListTodo([...listTodo, newTodo]);
+		const newListTodo = [...listTodo, newTodo];
+		setListTodo(newListTodo);
+	};
+
+	const deleteTodo = (todo) => {
+		const removeTodo = listTodo.filter((newTodo) => todo.id != newTodo.id);
+		setListTodo(removeTodo);
 	};
 
 	console.log({ listTodo });
@@ -21,7 +26,7 @@ const Home = () => {
 		<div className="col-md-6 offset-md-2">
 			<Form>
 				<Form.Group className="mb-3" controlId="formBasicText">
-					<Form.Label>Tu nombre</Form.Label>
+					<Form.Label>Cosas que hacer</Form.Label>
 					<Form.Control
 						type="text"
 						placeholder="Write here..."
@@ -29,8 +34,16 @@ const Home = () => {
 					/>
 				</Form.Group>
 				<Button variant="primary" type="button" onClick={handleClick}>
-					Click
+					Añadir
 				</Button>
+				{listTodo.map((todo, index) => (
+					<TodoList
+						key={index}
+						id={index}
+						deleteTodo={deleteTodo}
+						todo={todo}
+					/>
+				))}
 			</Form>
 		</div>
 	);
